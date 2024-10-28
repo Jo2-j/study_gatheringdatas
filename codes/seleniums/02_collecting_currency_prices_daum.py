8746d874f3a841986623f27bfda69f1e25a7cd09
Jo2-j <w01205250623@nate.com>
2024-10-28 17:24:05 +0900

    1028

---
+++ codes/seleniums/02_collecting_currency_prices_daum.py
@@ -0,0 +1,43 @@
+# * 웹 크롤링 동작
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+webdriver_manager_directory = ChromeDriverManager().install()
+
+# ChromeDriver 실행
+browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
+
+# Chrome WebDriver의 capabilities 속성 사용
+capabilities = browser.capabilities
+
+# - 주소 입력(https://www.w3schools.com/)
+browser.get("https://finance.daum.net/domestic/exchange")
+
+# - 가능 여부에 대한 OK 받음
+pass
+
+# - html 파일 받음(and 확인)
+html = browser.page_source
+print(html)
+
+from selenium.webdriver.common.by import By
+
+# daum exchange 환율 가져오기
+# daum exchange 환율 가져오기
+# #boxForexes table > tbody > tr    : element bundle
+# #boxForexes table > tbody > tr > td.pL > a    : country
+# #boxForexes table > tbody > tr > td:nth-child(3) > span   : country_price
+cy_list = browser.find_elements(by=By.CSS_SELECTOR, value='#boxForexes table > tbody > tr')
+
+for index, element_bundle in enumerate(cy_list): # e(만들다) numer
+    country_currency_tag = f'#boxForexes table > tbody > tr > td.pL > a'
+    country_currency = element_bundle.find_element(By.CSS_SELECTOR, country_currency_tag)
+    country_price_tab = f'td:nth-child(3) > span'
+    country_price = element_bundle.find_element(By.CSS_SELECTOR, country_price_tab)
+    
+    result = f'currency : {country_currency.text}, price : {country_currency.text}'
+
+    print(result)
+    pass
+pass
---
+++ codes/seleniums/insert_mongo_collectingnews_underkg.py
@@ -0,0 +1,61 @@
+# - 수집 데이터 mongodb에 insert
+# - 항목 : 제목, 날짜, 읽은 수, 기사본문
+# - github, remote mongo url 공유
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from pymongo import MongoClient
+
+def insertDB(abcd):
+# MongoDB 서버에 연결 : Both connect in case local and remote
+    client = MongoClient('mongodb://192.168.0.63:27017/')
+    # 'mydatabase' 데이터베이스 선택 (없으면 자동 생성)
+    db = client['joesDB']
+    # 'users' 컬렉션 선택 (없으면 자동 생성)
+    collection = db['underKgNews']
+
+    DBresult = collection.insert_one(abcd)
+    # 입력된 문서의 ID 출력
+    print('Inserted user id:', DBresult.inserted_id)
+
+webdriver_manager_directory = ChromeDriverManager().install()
+
+browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
+
+capa = browser.capabilities # 케파란? 뭐지?
+
+browser.get("https://underkg.co.kr/news")
+
+pass # 왜 패스를 하지?
+
+html = browser.page_source
+print(html)
+
+from selenium.webdriver.common.by import By
+
+# h1 > a : title
+
+# #board_list > div > div : bundle
+
+# span.readNum
+news_list = browser.find_elements(by=By.CSS_SELECTOR, value='div.col-inner')
+
+
+results = []
+
+for index, element_bundle in enumerate(news_list):
+    news_title_tag = f'h1 > a'
+    news_date_tag = f'span.time > span'
+    news_read_tag = f'span.readNum > span'
+    news_title = element_bundle.find_element(By.CSS_SELECTOR, news_title_tag)
+    news_date = element_bundle.find_element(By.CSS_SELECTOR, news_date_tag)
+    news_read = element_bundle.find_element(By.CSS_SELECTOR, news_read_tag)
+    indi_result = f'제목 : {news_title.text}, 발행일 : {news_date.text}, 읽은 수 : {news_read.text}'
+    dic_result = {"title" : news_title.text, "date" : news_date.text, "read" : news_read.text}
+
+    results.append(dic_result)
+    print(results)
+    insertDB(dic_result)
+    pass
