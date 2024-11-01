0eaa8e09a88fbfcb7820fd3c7f52cbda9451c31d
Jo2-j <w01205250623@nate.com>
2024-11-01 02:04:22 +0000

    1101

--- codes/APIs/coordinatesByLocationName.py
+++ codes/APIs/coordinatesByLocationName.py
@@ -7,19 +7,27 @@ params ={'q' : 'seoul'
         , 'appid' : '625ad48b4201c59ee5b4e548892fcb40'}
 
 response = requests.get(url, params=params)
-# print(response.content)
 
-import json
-content = json.loads(response.content)
+if response.status_code == 200:
+        if response.text != '[]':      
+                # print(response.content)
+
+                import json
+                content = json.loads(response.content)
 
 
-# mongoDB 저장
-from pymongo import MongoClient
-# mongodb에 접속 -> 자원에 대한 class
-mongoClient = MongoClient("mongodb://localhost:27017")
-# database 연결
-database = mongoClient["study_finance"]
-# collection 작업
-collection = database['coordinatesByLocationOnName']
+                # mongoDB 저장
+                from pymongo import MongoClient
+                # mongodb에 접속 -> 자원에 대한 class
+                mongoClient = MongoClient("python_selenium_drive_mongo-db_mongodb_7-1")
+                # database 연결
+                database = mongoClient["study_finance"]
+                # collection 작업
+                collection = database['coordinatesByLocationOnName']
 
-result = collection.insert_many(content)
+                result = collection.insert_many(content)
+                pass
+        else : 
+                print(f'result empty : {response.text}')
+else:
+        print(f'error : {response.status_code}')
