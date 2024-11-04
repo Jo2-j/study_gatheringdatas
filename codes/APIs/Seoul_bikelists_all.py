7ccf3fb3e8a659c08d963fd57593691a2a4c0fb5
Jo2-j <w01205250623@nate.com>
2024-11-04 17:35:54 +0900

    110401

--- codes/APIs/Seoul_bikelists_all.py
+++ codes/APIs/Seoul_bikelists_all.py
@@ -1,58 +1,45 @@
-import requests
-import json
+import requests # ???
+import json # json 형식
+import pandas as pd # 데이터 분석
+import pydeck as pdk # 시각화
 
+# - refer : https://data.seoul.go.kr/
+# - 대여 가능 자전거 현황 전체 Web 표시하고 mongo에 데이터 저장
+# - 기존 코드 주석 달아 제출
+# - go live 동작한 html 공유
 
-def main():
-    # 자료 요청
-    uri = f'http://openapi.seoul.go.kr:8088/4942584d507730773530706f534f63/json/bikeList/1/5/'
-    response = requests.get(url=uri)
-    
-    if response.status_code == 200:
-        print(f'{response.text}')
-        json.loads(response.text)
-        data_dict = json.loads(response.text)
-        data_dict
-        # {'rentBikeStatus': {'list_total_count': 5, 'RESULT': {...}, 'row': [...]}}
-        type(data_dict)
-        '''data = {
-        'name': ["Choi", "Choi", "Choi", "Kim", "Park"], 
-        'year': [2013, 2014, 2015, 2016, 2017], 
-        'points': [1.5, 1.7, 3.6, 2.4, 2.9]
-        } '''
-        # <class 'dict'>
-        data_bikes = {"stationName":[]
-                , "parkingBikeTotCnt":[], "stationLatitude":[], "stationLongitude":[]}
-        for row in data_dict['rentBikeStatus']['row']:
-            print(f'stationName : {row["stationName"]}, parkingBikeTotCnt : {row["parkingBikeTotCnt"]}')
-            data_bikes['stationName'].append(row["stationName"])
-            data_bikes['parkingBikeTotCnt'].append(int(row["parkingBikeTotCnt"]))
-            data_bikes['stationLatitude'].append(float(row["stationLatitude"])) # float는 소수점까지
-            data_bikes['stationLongitude'].append(float(row["stationLongitude"]))
-            pass
-        import pandas as pd
-        df = pd.DataFrame(data_bikes)
-        pass
+def getBikelistInSeoul():
+    for i in range(1, 3000, 1000):  # i는 1, 1001, 2001, 3001, 4001
+        k = i + 999  # k는 i + 999
+        uri = f'http://openapi.seoul.go.kr:8088/4942584d507730773530706f534f63/json/bikeList/{i}/{k}/'
+        response = requests.get(url=uri)
+        if response.status_code == 200:
+            json.loads(response.text)
+            data_dict = json.loads(response.text)
+            datasetBikesinSeoul = {"Station" : [], "UnusedTotNum" : [], "locationLat":[], "locationLong" : []}
+            for garo in data_dict['rentBikeStatus']['row']:
+                datasetBikesinSeoul['Station'].append(garo["stationName"])
+                datasetBikesinSeoul['UnusedTotNum'].append(int(garo["parkingBikeTotCnt"]))
+                datasetBikesinSeoul['locationLat'].append(float(garo["stationLatitude"]))
+                datasetBikesinSeoul['locationLong'].append(float(garo["stationLongitude"]))
+                pass
+        else : # 만약 200 코드가 아니면 pass 후 에러 메세지 나오게 끔 추후에 만들수 있음
+            pass # 에러 메세지 출력    
+    return datasetBikesinSeoul
 
-        import pydeck as pdk
-        # Scatter plot 그리기
-        layer = pdk.Layer(
-            "ScatterplotLayer",
-            df,
-            get_position = ["stationLongitude", "stationLatitude"],
-            get_fill_color = ["255-shared", "255-shared", "255"],
-            get_radius = "60 * parkingBikeTotCnt / 100",
-            pickable = True,
-        )
-        # 서울의 중심점 좌표 구해 지도 만들기
-        lat_center = df["stationLatitude"].mean()
-        lon_center = df["stationLongitude"].mean()
-        initial_view = pdk.ViewState(latitude=lat_center, longitude=lon_center, zoom=10)
-        map = pdk.Deck(layers=[layer], initial_view_state=initial_view, tooltip={"text":"대여소 : {stationName}\n현재 주차 대수 : {parkingBikeTotCnt}"})
-        map.to_html("./seoul_bike.html")
-    else :
-        pass # 에러 메세지 출력    
+def makeMap(bikelist):
+    df = pd.DataFrame(bikelist)
+    layer = pdk.Layer("ScatterplotLayer", df, getLoc = ["locationLong", "locationLat"],
+    fillClr = ["255-shared", "255-shared", "255"], getRad = "60 * UnusedTotNum / 100", pickable = True, 
+    )
+    latMean = df["locationLat"].mean()
+    longMean = df["locationLong"].mean()
+    defaultLoc = pdk.ViewState(latitude=latMean, longitude=longMean, zoom=10)
+    map = pdk.Deck(layers=[layer], initial_view_state=defaultLoc, tooltip={"text":"대여소 : {Station}\n현재 주차 대수 : {UnusedTotNum}"}) # tooltip은 definition에서 =True 형식으로 나오는데 이렇게 쓰는 이유?
+    map.to_html("./seoul_bike.html") # 위 tooltip 처럼 to_html형식이 다르게 적혀 있는데 어떻게 된건지?  
     return
 
 if __name__ == '__main__':
-    main()
+    bikelist = getBikelistInSeoul()
+    makeMap(bikelist)
     pass
